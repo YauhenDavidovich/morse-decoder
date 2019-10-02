6f638bc08030020723cf659711e554ce26b37336
unknown <davidovich336@gmail.com>
2019-10-02 14:51:43 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,26 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) {    
+    let decode_msg = '';
+    let binary_converted_msg = ' '; 
+    let morse_type_list = [];
+    let letter_order_number = 0;  
+    for (let i = 0; i < expr.length; i += 10) { 
+        binary_converted_msg = binary_converted_msg.replace(/00/g, "");          
+        binary_converted_msg = binary_converted_msg.replace(/10/g, ".");
+        binary_converted_msg = binary_converted_msg.replace(/11/g, "-");
+        morse_type_list.push(binary_converted_msg);
+        if (morse_type_list[letter_order_number] === "**********") {
+            decode_msg += ' '
+        } else {
+            decode_msg += MORSE_TABLE[morse_type_list[letter_order_number]];
+        } 
+        letter_order_number += 1            
+    } 
+    return decode_msg;          
 }
-
 module.exports = {
     decode
 }
+
