821583c75a0aae6c373ba262d048efe5bece60c4
Пользователь <davidovich336@gmail.com>
2019-10-07 14:43:27 +0300

    feat: here my solution for task

--- src/index.js
+++ src/index.js
@@ -1,62 +1,59 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
-function decode(expr) {    
-    let decode_msg = '';
-    let binary_converted_msg = ' '; 
-    let morse_type_list = [];
-    let letter_order_number = 0;  
-    for (let i = 0; i < expr.length; i += 10) { 
-        binary_converted_msg = binary_converted_msg.replace(/00/g, "");          
-        binary_converted_msg = binary_converted_msg.replace(/10/g, ".");
-        binary_converted_msg = binary_converted_msg.replace(/11/g, "-");
-        morse_type_list.push(binary_converted_msg);
-        if (morse_type_list[letter_order_number] === "**********") {
-            decode_msg += ' '
+function decode(expr) {
+    let decode_msg = ''; //it will be result
+    let letter = " "
+    let splitMsg = expr.match(/.{1,10}/g); //divide the binary string by 10 characters 
+    let splitMsg_order_number = 0; // initial counter value
+    for (let i = 0; i < splitMsg.length; i += 1) {
+        letter = (((splitMsg[splitMsg_order_number].replace(/10/g, ".").replace(/11/g, "-").replace(/00/g, "")))); //get rid of the extra zeros and convert the binary record to " dot-dash"
+        if (letter == "**********") {
+            decode_msg += ' '//check for spaces
         } else {
-            decode_msg += MORSE_TABLE[morse_type_list[letter_order_number]];
-        } 
-        letter_order_number += 1            
-    } 
-    return decode_msg;          
+            decode_msg += MORSE_TABLE[letter];//replace with the value from the dictionary
+        }
+        splitMsg_order_number += 1;//increase counter
+
+    }
+    return decode_msg;//here morse-decoded string
 }
 module.exports = {
     decode
 }
-
